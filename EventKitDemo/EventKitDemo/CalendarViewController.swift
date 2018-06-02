bc73982efa497eba4095c23e64214b274de10b59
Rob Timpone <rtimpone@gmail.com>
2018-06-02 15:34:09 -0500

    Adding ability to change dates

--- EventKitDemo/EventKitDemo/CalendarViewController.swift
+++ EventKitDemo/EventKitDemo/CalendarViewController.swift
@@ -12,47 +12,37 @@ import UIKit
 class CalendarViewController: UIViewController {
     
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var selectedDateLabel: UILabel!
+    
     let store = EKEventStore()
+    var selectedDate = Date()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectedDate = defaultDate()
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         checkForPermission()
+    }
+    
+    @IBAction func previousDayAction(_ sender: UIButton) {
+        let date = previousDate(before: selectedDate)
+        changeDate(to: date)
+    }
+    
+    @IBAction func nextDayAction(_ sender: UIButton) {
+        let date = nextDate(after: selectedDate)
+        changeDate(to: date)
     }
 }
 
 private extension CalendarViewController {
     
-    func checkForPermission() {
+    func loadCalendarData(for date: Date) {
         
-        let status = EKEventStore.authorizationStatus(for: .event)
-        switch status {
-        case .notDetermined:
-            requestPermission()
-        case .authorized:
-            loadCalendarData()
-        case .denied, .restricted:
-            showPermissionDeniedAlert()
-        }
-    }
-    
-    func requestPermission() {
-        
-        store.requestAccess(to: .event) { permissionGranted, error in
-            DispatchQueue.main.async {
-                if permissionGranted {
-                    self.loadCalendarData()
-                }
-                else {
-                    self.showPermissionDeniedAlert()
-                }
-            }
-        }
-    }
-    
-    func loadCalendarData() {
-        
-        let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
+        let startOfDay = Calendar.current.startOfDay(for: date)
         let endOfDay = Calendar.current.date(byAdding: .day, value: 1, to: startOfDay)!
         
         let calendars = store.calendars(for: .event)
@@ -69,7 +59,16 @@ private extension CalendarViewController {
             df.dateFormat = "h:mm a"
             let localStartTimeString = df.string(from: startTime)
             
-            printToTextView("\(title) \(localStartTimeString)")
+            printToTextView(title)
+            
+            printToTextView(event.calendar.title)
+            
+            if let location = event.location, !location.isEmpty {
+                printToTextView(location)
+            }
+            
+            printToTextView(localStartTimeString)
+            printBlankLineToTextView()
         }
     }
     
@@ -77,14 +76,65 @@ private extension CalendarViewController {
         //show an alert directing the user to Settings
     }
     
-    func printSelectedDateToTextView() {
+    func updateDateLabe(for date: Date) {
+        let df = DateFormatter()
+        df.dateFormat = "MMMM d, yyyy"
+        selectedDateLabel.text = df.string(from: date)
+    }
+    
+    func changeDate(to date: Date) {
+        selectedDate = date
+        updateDateLabe(for: date)
+        clearTextView()
+        loadCalendarData(for: date)
+    }
+    
+    // MARK - Permissions
+    
+    func checkForPermission() {
         
+        let status = EKEventStore.authorizationStatus(for: .event)
+        switch status {
+        case .notDetermined:
+            requestPermission()
+        case .authorized:
+            loadCalendarData(for: selectedDate)
+        case .denied, .restricted:
+            showPermissionDeniedAlert()
+        }
+    }
+    
+    func requestPermission() {
+        
+        store.requestAccess(to: .event) { permissionGranted, error in
+            DispatchQueue.main.async {
+                if permissionGranted {
+                    self.loadCalendarData(for: self.selectedDate)
+                }
+                else {
+                    self.showPermissionDeniedAlert()
+                }
+            }
+        }
+    }
+    
+    // MARK - Text View
+    
+    func clearTextView() {
+        textView.text = ""
     }
     
     func printToTextView(_ text: String) {
         let currentText = textView.text!
         textView.text = currentText + text + "\n"
     }
+    
+    func printBlankLineToTextView() {
+        let currentText = textView.text!
+        textView.text = currentText + "\n"
+    }
+    
+    // MARK - Date Management
     
     func defaultDate() -> Date {
         let now = Date()
@@ -98,7 +148,7 @@ private extension CalendarViewController {
         return nextDate
     }
     
-    func prevoiusDate(before date: Date) -> Date {
+    func previousDate(before date: Date) -> Date {
         guard let previousDate = Calendar.current.date(byAdding: .day, value: -1, to: date) else {
             fatalError("Unable to create date one day before \(date)")
         }
