dad943d3a54f98bf013ccb168dde382099de169d
Rob Timpone <rtimpone@gmail.com>
2018-06-02 12:34:35 -0500

    Adding new Xcode project

---
+++ EventKitDemo/EventKitDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  EventKitDemo
+//
+//  Created by Rob Timpone on 6/2/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
